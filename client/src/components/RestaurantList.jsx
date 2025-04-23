4240a267232d96f22d743dc34507276130f092ac
Rosa <kosolrosa@gmail.com>
2025-04-23 17:31:13 +1000

    Implemented Delete Restaurant functionality

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -18,6 +18,21 @@ const RestaurantList = (props) => {
         fetchData();
     },[])
 
+    const handleUpdate = () => {
+        console.log("")
+    };
+
+    const handleDelete = async (id) => {
+        try {
+            const response = await RestaurantFinder.delete(`/${id}`);
+            setRestaurants(restaurants.filter(restaurant => {
+                return restaurant.id !== id
+            }))
+        } catch (err) {
+
+        }
+    }
+
   return (
     <div>
       <table className="table">
@@ -39,8 +54,8 @@ const RestaurantList = (props) => {
                         <td>{restaurant.location}</td>
                         <td>{"$".repeat(restaurant.price_range)}</td>
                         <td>Reviews</td>
-                        <td><button className="btn btn-warning">Update</button></td>
-                        <td><button className="btn btn-danger">Delete</button></td>
+                        <td><button onClick={() => handleUpdate(restaurant.id)} className="btn btn-warning">Update</button></td>
+                        <td><button onClick={() => handleDelete(restaurant.id)} className="btn btn-danger">Delete</button></td>
                     </tr>
                     )
                 })}
