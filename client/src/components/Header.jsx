97b5647f2826e8bd46a43038b0f561877192f09a
Rosa <kosolrosa@gmail.com>
2025-04-26 19:31:27 +1000

    Cleaned up files

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-      <h1>Restaurant Finder</h1>
+      <h1 className="font-bold text-blue-500">Restaurant Finder</h1>
     </div>
   )
 }
